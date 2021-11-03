b475d929f5986857c5be8e1573cd525a0290ac1f
Matthew Banse-Fay <matthew.bansefay@gmail.com>
2021-11-02 19:41:36 -0700

    Add HTML/Javascript code for triangle detection.
    Co-authored-by: Jack <JTSkelton@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-  
+  $("#triangle-form").submit(function(event) {
+    event.preventDefault();
+    let side1 = parseInt($("#side1").val()); 
+    let side2 = parseInt($("#side2").val());
+    let side3 = parseInt($("#side3").val());
+
+    if (((side1 + side2) <= side3) || ((side2 + side3) <= side1) || ((side3 + side1) <= side2)) {
+      $(".result").text("This is not a triangle");
+    } else if (side1 === side2 && side2 === side3 && side1 === side3) {
+      $(".result").text("This is an equilateral triangle");
+    } else if (side1 === side2 || side2 === side3 || side1 === side3) {
+      $(".result").text("This is an iscosceles triangle");
+    } else if (side1 !== side2 && side2 !== side3 && side1 !== side3) {
+      $(".result").text("This is an scalene triangle");
+    } 
+  });
 });
